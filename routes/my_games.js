01d022a2a4e5108fca972fc8b3931925958d42a7
Jan <jan.leon.voelker@gmail.com>
2020-11-23 16:59:20 +0100

    Route to games-by-name-search-results

--- routes/my_games.js
+++ routes/my_games.js
@@ -4,21 +4,27 @@ const User = require('../models/User');
 
 
 // general game search by game's name
-app.get('/my_games/search', (req, res) => {
-  //req params
-  const searchedGame = games.filter(game => game // we might to have to change that all to the API ;D
-    .title
-    .toLowerCase()
-    .includes(req.query.q.toLowerCase())
-    );
-    console.log(searchedGame);
-    res.render('/my_games/games_list', {games:searchedGame})
-})
+app.get('/my_games/', (req, res) => {
+res.render('index')
+});
 
 // result page of games searched by name
-app.get('/my_games/search', (req, res) => {
-res.render()
-});
+app.get('/my_games/game_search_by_name', (req, res) => {
+  const searchedGame = req.query.q
+    .toLowerCase()
+    .includes(req.query.q.toLowerCase())
+    console.log(searchedGame);
+axios.get(`https://api.boardgameatlas.com/api/search?name=${searchedGame}&client_id=Bb6pHO9yhc&fuzzy_match`)
+    .then(game => {
+     console.log(game);
+    res.render('search_results', {games:searchedGame.games}) // do I need to add /my_games in front of the search results?
+  })
+    .catch(err => console.log('Error while searching for game by name occured: ', err));
+})
+
+
+
+
 
 
 // list of my games
