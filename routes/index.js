fb64d029b305451818eede69d2ee80251fbc7f76
Jan <jan.leon.voelker@gmail.com>
2020-11-26 11:01:05 +0100

    not going to need those

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,39 @@ const express = require('express');
 const router  = express.Router();
 const axios = require('axios');
 
+
+
+// const generateOneGame = theOneGame => {
+
+//   axios
+
+//     .get(`https://restcountries.eu/rest/v2/name/${countryName}`)
+
+//     .then(response => {
+
+//       console.log('Response from API is: ', response);
+
+//       const countryDetail = response.data[0];
+
+//       console.log('time is ', countryDetail);
+
+//     })
+
+//     .catch(err => console.log(err));
+
+// };
+
+ 
+
+// document.getElementById('generate-game-btn').addEventListener('click', () => {
+
+//   const time = document.getElementById('time').value;
+//   console.log(time)
+
+//   // getCountryInfo(userInput);
+
+// });
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   const loggedInUser = req.user;
@@ -19,8 +52,8 @@ router.get('/game_details', (req, res) => {
     if (time) {
       url += time;
     }
-    if (players) {
-      url += players;
+    if (player) {
+      url += player;
     }
     if (age) {
       url += age;
